f4849f6c13e94bcfd9abe921563888300f8c9f24
박수정 <sjj083015@gmail.com>
2023-07-12 18:56:33 +0900

    [MOD] 1.주석 추가(update)

--- src/ch03/course02/VIPCustomer.java
+++ src/ch03/course02/VIPCustomer.java
@@ -5,6 +5,10 @@ public class VIPCustomer extends Customer {
 	double salesRatio;
 	
 	public VIPCustomer() {
+		// 상속을 받은 경우, 명시적으로 상위 클래스의 생성자 호출부가 없는 경우 컴파일러가 super() 를 추가해줌.
+		// 때문에 별도의 상위 클래스 생성자 호출부가 없어도, 상위 클래스의 생성자 호출이 이루어지는 것임.
+//		super();
+		
 		customerGrade = "VIP";
 		bonusRatio = 0.05;			// 5% 보너스 포인트
 		salesRatio = 0.1;			// 10% 할인율
